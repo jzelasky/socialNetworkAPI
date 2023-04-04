4e5c6f722bc9f5cd0a4e91f2582644a64a8f0e1b
Jessica Zelasky <jzelasky@gmail.com>
2023-04-04 11:59:01 -0400

    added reaction schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,14 +1,21 @@
 const mongoose = require('mongoose');
 
 //Create a virtual called reactionCount that retrieves the length of the thought's reactions array fireld on query
-//Add reaction schema (see readme for details), to be used as the reaction field's subdocument schema in the Thought model
+
+const reactionSchema = new mongoose.Schema({
+
+  reactionId: { }, //use mongoose's ObjectId data type, default value is set to a new ObejctId
+  reactionBody: { type: String, required: true }, //add 280 character max
+  username: { type: String, required: true}, //user that created the reaction, what needs to be referenced where?
+  createdAt: { type: Date }, //set default value to the current timestamp, use a getter emthod to format the timestamp on query
+})
 
 const thoughtSchema = new mongoose.Schema({
 
-  thoughtText: { type: String, required: true }, //add must be between 1 and 280 characters
+  thoughtText: { type: String, required: true }, //add 280 character max
   createdAt: { type: Date }, //set default value to the current timestamp, use a getter emthod to format the timestamp on query
-  username: { type: String, required: true}, // the user that created the though, what needs to be referenced?
-  reactions: {} //array of nested documents crated with the reactionSchema
+  username: { type: String, required: true}, // the user that created the thought, what needs to be referenced where?
+  reactions: [reactionSchema]
   
 });
 
